6fb078e0eed5f0f17c356c9b5140a82a50da5bc0
Haydn Jones <haydnjonest@gmail.com>
2020-12-18 15:52:01 -0700

    Fixed some config issues.

--- src/Config.cpp
+++ src/Config.cpp
@@ -11,23 +11,22 @@ Config::Config(int argc, char **argv)
    parser.config_formatter(std::make_shared<CLI::ConfigTOML>());
 
    // clang-format off
-   parser.add_option("--slopePath",     this->slopePath,      "Path to slope data")->check(CLI::ExistingFile);
-   parser.add_option("--twiPath",       this->twiPath,        "Path to TWI data")->check(CLI::ExistingFile);
-   parser.add_option("--soilTypePath",  this->soilTypePath,   "Path to soil type data")->check(CLI::ExistingFile);
-   parser.add_option("--soilDepthPath", this->soilDepthPath,  "Path to soil depth data")->check(CLI::ExistingFile);
-   parser.add_option("--dusafPath",     this->dusafPath,      "Path to dusaf data")->check(CLI::ExistingFile);
-   parser.add_option("--probslopePath", this->probslopePath,  "Path to probslope data")->check(CLI::ExistingFile);
-   parser.add_option("--seed",          this->seed,           "Seed for RNG", true);
-   parser.add_option("--numLandslides", this->num_landslides, "The number of landslides to simulate", true);
-
-   parser.add_option("--outputPath",    this->outputPath,    "Path to output directory");
+   parser.add_option("--slopePath",     this->slopePath,      "Path to slope data")->check(CLI::ExistingFile)->required();
+   parser.add_option("--twiPath",       this->twiPath,        "Path to TWI data")->check(CLI::ExistingFile)->required();
+   parser.add_option("--soilTypePath",  this->soilTypePath,   "Path to soil type data")->check(CLI::ExistingFile)->required();
+   parser.add_option("--soilDepthPath", this->soilDepthPath,  "Path to soil depth data")->check(CLI::ExistingFile)->required();
+   parser.add_option("--dusafPath",     this->dusafPath,      "Path to dusaf data")->check(CLI::ExistingFile)->required();
+   parser.add_option("--probslopePath", this->probslopePath,  "Path to probslope data")->check(CLI::ExistingFile)->required();
+   parser.add_option("--seed",          this->seed,           "Seed for RNG", true)->required();
+   parser.add_option("--numLandslides", this->num_landslides, "The number of landslides to simulate", true)->required();
+   parser.add_option("--outputPath",    this->outputPath,     "Path to output directory")->required();
    // clang-format on
 
    try {
       parser.parse(argc, argv);
    } catch (const CLI::ParseError &e) {
       parser.exit(e);
-      std::cout << "\nDumping config to default_config.toml" << std::endl;
+      std::cout << "\nWriting out a config file, called 'default_config.toml', for you to fill in." << std::endl;
       this->dumpConfigFile("default_config.toml");
       exit(EXIT_FAILURE);
    }
