a92dcb601bc7911e0bad8e1e2a7272f99d9aef2c
Ohunter <ohoffs@gmail.com>
2021-02-06 10:29:46 +0100

    Added explanation and reorganized python test script

--- utils/test.py
+++ utils/test.py
@@ -1,12 +1,24 @@
+"""
+This file is primarily used to compute the MSE between two rasters.
+It expects a .asc format and CAN NOT deal with raw tiff files.
+You can either pass two files as parameters to the script or you can define their paths in the paths variable.
+
+Usage: 
+`test.py path/to/raster_1.asc path/to/raster_2.asc`
+"""
+
+import sys
 import numpy as np
 
-p1 = "build/Study_Test_UNIPV/prob_failure.asc"
-p2 = "../PRIMULATests/Tests/Study_Test_UNIPV/reference_probls.asc"
+def main(argv):
+    a, b, = [np.loadtxt(x, skiprows=6) for x in argv]
 
-a = np.loadtxt(p1, skiprows=6)
-b = np.loadtxt(p2, skiprows=6)
+    print(f"MSE: {((a - b) ** 2).mean()}")
 
-# a = a[b!= -9999]
-# b = b[b!= -9999]
+if __name__ == '__main__':
+    paths = []
 
-print(((a - b) ** 2).mean())
+    if len(sys.argv) > 1:
+        main(sys.argv[1:])
+    else:
+        main(paths)
