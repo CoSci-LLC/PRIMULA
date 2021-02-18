4a2a9f5b0493c83c9ca6c9e19a51d6255acb3a90
Owen Parkins <oparkins@gmail.com>
2021-02-18 18:22:43 -0500

    PRIMULA Release v1.0.1
    - Rainfall now command line arg
    - Added Validations

--- src/Config.hpp
+++ src/Config.hpp
@@ -1,21 +1,21 @@
 /**
  *  Copyright (c) 2020-2021 CoSci LLC, USA <software@cosci-llc.com>
- *  
+ *
  *  This file is part of PRIMULA.
- *  
+ *
  *  PRIMULA is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- *  
+ *
  *  PRIMULA is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with PRIMULA.  If not, see <https://www.gnu.org/licenses/>.
-*/
+ */
 
 #pragma once
 
@@ -50,6 +50,8 @@ private:
    std::string defaultExtension = ".tif";
 
    std::string rastOutPath;
+
+   double rainfall_ = 0.160; // [m/day]
 
    size_t num_landslides = 100;
    size_t seed           = 333;
